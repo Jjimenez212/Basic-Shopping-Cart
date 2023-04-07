6ce00ed16f024af2664a1b2285bf07e5d25c8c68
Juan Jimenez <bozz212@outlook.com>
2023-04-06 19:05:29 -0700

    pushing

---
+++ src/static-data.js
@@ -0,0 +1,17 @@
+let items = [
+    {
+        id: 0,
+        name: "Space Ship X14",
+        descriptions: "It can go through the Solar System, no problem.",
+        price: 1000000000.00,
+        image: '../images/X14.jpg'
+    },
+    {
+        id: 1,
+        name: "Space Ship New Havemn",
+        descriptions: "It can reach the next start if tempted.",
+        price: 1000000000.00
+    },
+];
+
+export {items};
